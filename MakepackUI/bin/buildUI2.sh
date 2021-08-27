a45fbbb9cc65e44f83055bc2a191938b9ba70d20
student <samuelliao22@gmail.com>
2021-08-27 11:27:37 -0700

    I added makepackui command

--- MakepackUI/bin/buildUI2.sh
+++ MakepackUI/bin/buildUI2.sh
@@ -5,4 +5,4 @@
 #/src/main/java/newpackage
 javac -classpath json-simple-1.1.1.jar ../src/main/java/newpackage/NewJFrame.java -d . -Xlint:unchecked || exit
 jar cmf makepackui.mf ./makepackui.jar newpackage/*.class ../src/main/resources/* || exit
-java -jar makepackui.jar
+java -jar makepackui.jar & 
--- testsets/bin/testMakepack.sh
+++ testsets/bin/testMakepack.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd $LABTAINER_DIR/scripts/labtainer-student
 rm -f $LABTAINER_DIR/labpacks/testpack.labpack
+cd $LABTAINER_DIR/MakepackUI/bin
+./buildUI2.sh 
+sleep 5
+cd $LABTAINER_DIR/scripts/labtainer-student
 SimLab.py -vv MakepackUI
 # use diff to compare new labpack to expected results
 DIFF=$(diff $LABTAINER_DIR/labpacks/testpack.labpack $LABTAINER_DIR/testsets/makepack/expected/testpack.labpack)
